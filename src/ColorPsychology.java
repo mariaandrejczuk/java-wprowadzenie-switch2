de86560f3d39aa1b677ca3eab31901c0f39fd88f
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2020-12-20 18:18:19 +0100

    #3 implementacja zadania 2 - ColorPsychology

---
+++ src/ColorPsychology.java
@@ -0,0 +1,18 @@
+public class ColorPsychology {
+    public static void main(String[] args) {
+        char color = 'n';
+        switch (color) {
+            case 'c':
+                System.out.println("pomidor");
+                break;
+            case 'z':
+                System.out.println("trawa");
+                break;
+            case 'n':
+                System.out.println("morze");
+                break;
+            default:
+                System.out.println("brak skojarzeń");
+        }
+    }
+}
